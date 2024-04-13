a12ee4822934677abea84944558f792ef3c5e524
Aleslammer <junkmail.lordcheese@gmail.com>
2024-04-13 12:53:51 -0500

    chore: only if hit show animation

--- scripts/PsychicPowers/Psy_Avenger.js
+++ scripts/PsychicPowers/Psy_Avenger.js
@@ -277,7 +277,7 @@ on("chat:message", function (msg) {
             }
 
             params.hits > 0 ? sendChatMessage += `\n--+Penetration:|${params.penetration}` : null;
-            sendChatMessage += `\n--vtoken|${params.targetID} bomb-fire`
+            params.hits > 0 ? sendChatMessage += `\n--vtoken|${params.targetID} bomb-fire`
 
             var awValue = "";
             for (lcv = 0; lcv < params.hits; lcv++) {
